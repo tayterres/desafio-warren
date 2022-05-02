83611456659c20e9ae94e27e3d1bfbbfc92b4ea6
tayterres <taycr9@hotmail.com git config --global user.name tayterresgit config --global user.email taycr9@hotmail.com>
2022-05-02 00:13:28 -0300

    primeiro

---
+++ Desafio01/Desafio01/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Desafio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 1000000; i++)
+            {
+                if (i % 10 != 0)
+                {
+                    var numero = i;
+                    var invertido = 0;
+                    while (numero > 0)
+                    {
+                        invertido = invertido * 10 + numero % 10;
+                        numero /= 10;
+                    }
+
+                    var soma = i + invertido;
+                    var impar = soma % 2;
+
+                    if (impar > 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+            }
+        }
+    }
+}
